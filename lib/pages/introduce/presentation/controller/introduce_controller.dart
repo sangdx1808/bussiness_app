ebb89bd852e19244c8cf857515a13aab64a9d28c
DXS1808 <89695455+DXS1808@users.noreply.github.com>
2023-09-29 16:16:38 +0700

    remove code

--- lib/pages/introduce/presentation/controller/introduce_controller.dart
+++ lib/pages/introduce/presentation/controller/introduce_controller.dart
@@ -1,9 +1,3 @@
 import 'package:bussiness_app/index.dart';
 
-class IntroduceController extends BaseController {
-  @override
-  void onReady() {
-    // TODO: implement
-    super.onReady();
-  }
-}
+class IntroduceController extends BaseController {}
--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -5,10 +5,6 @@ import '../index.dart';
 class AppPages {
   static final routes = [
     GetPage(
-      name: AppRouter.home,
-      page: () => const HomeScreen(),
-    ),
-    GetPage(
       binding: IntroduceBinding(),
       name: AppRouter.introduce,
       transition: Transition.rightToLeft,
